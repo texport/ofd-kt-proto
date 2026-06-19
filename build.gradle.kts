8757593757499c7a7bbfebdaffbe8db3292eecc5
Sergey Ivanov <ivanov.sergey.ekb@gmail.com>
2026-06-19 23:58:29 +0700

    Eliminate legacy apply plugin syntax in favor of modern plugins DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,11 @@
 plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.protobuf) apply false
-    alias(libs.plugins.detekt)
+    alias(libs.plugins.detekt) apply false
 }
 
 allprojects {
     repositories {
         mavenCentral()
-    }
-}
-
-subprojects {
-    apply(plugin = "io.gitlab.arturbosch.detekt")
-
-    detekt {
-        config.setFrom(files("${rootDir}/config/detekt/detekt.yml"))
-        buildUponDefaultConfig = true
-        allRules = true
-    }
-
-    tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
-        exclude("**/build/generated/**")
-        exclude("**/generated/source/proto/**")
     }
 }
--- proto-v203/build.gradle.kts
+++ proto-v203/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.protobuf)
+    alias(libs.plugins.detekt)
 }
 
 group = "kz.kazakhtelecom"
@@ -33,4 +34,15 @@ protobuf {
             }
         }
     }
+}
+
+detekt {
+    config.setFrom(files("${rootDir}/config/detekt/detekt.yml"))
+    buildUponDefaultConfig = true
+    allRules = true
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    exclude("**/build/generated/**")
+    exclude("**/generated/source/proto/**")
 }
