f437a6890ca62cd6e8b266d2a81d62d140ed807c
Sergey Ivanov <ivanov.sergey.ekb@gmail.com>
2026-06-20 18:02:58 +0700

    Configure Maven Central publishing, sign plugin, bilingual README, and GitHub Actions CI workflow

--- proto-v203/build.gradle.kts
+++ proto-v203/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.protobuf)
     alias(libs.plugins.detekt)
+    `maven-publish`
+    signing
 }
 
 group = "kz.kazakhtelecom"
@@ -19,8 +21,67 @@ kotlin {
     jvmToolchain(17)
 }
 
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
 base {
     archivesName.set("ofd-kt-proto-v203")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            
+            pom {
+                name.set("ofd-kt-proto-v203")
+                description.set("Protobuf generated entities for KazakhTelecom OFD Protocol v2.0.3")
+                url.set("https://github.com/texport/ofd-kt-proto")
+                
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                
+                developers {
+                    developer {
+                        id.set("texport")
+                        name.set("Sergey Ivanov")
+                        email.set("ivanov.sergey.ekb@gmail.com")
+                    }
+                }
+                
+                scm {
+                    connection.set("scm:git:git://github.com/texport/ofd-kt-proto.git")
+                    developerConnection.set("scm:git:ssh://github.com:texport/ofd-kt-proto.git")
+                    url.set("https://github.com/texport/ofd-kt-proto")
+                }
+            }
+        }
+    }
+    
+    repositories {
+        maven {
+            name = "OSSRH"
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            
+            credentials {
+                username = project.findProperty("ossrhUsername")?.toString() ?: System.getenv("OSSRH_USERNAME")
+                password = project.findProperty("ossrhPassword")?.toString() ?: System.getenv("OSSRH_PASSWORD")
+            }
+        }
+    }
+}
+
+signing {
+    isRequired = false
+    sign(publishing.publications["mavenJava"])
 }
 
 protobuf {
