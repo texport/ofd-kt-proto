ae941bd6a10ff8318470fb4936b833d836b03db5
Sergey Ivanov <ivanov.sergey.ekb@gmail.com>
2026-06-20 17:50:03 +0700

    Align AGP, modularize offline-queue/delivery/renderer, and fix Android SDK imports

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
+
 rootProject.name = "ofd-kt-proto"
 include("proto-v203")
 project(":proto-v203").name = "ofd-kt-proto-v203"
